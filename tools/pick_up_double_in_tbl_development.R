39c44e1f610721bfbe93cc20a051ac6c7676d5a9
koiti-yano <koiti@komazawa-u.ac.jp>
2024-12-25 17:03:52 +0900

    Rev

--- tools/pick_up_double_in_tbl_development.R
+++ tools/pick_up_double_in_tbl_development.R
@@ -4,13 +4,14 @@
 # https://cran.rstudio.com/web/packages/tsibble/vignettes/intro-tsibble.html
 # https://stackoverflow.com/questions/62583977/why-is-select-if-is-double-returning-dates
 
-#require(dplyr)
-#require(nycflights13)
+require(tidyverse)
+#install.packages("nycflights13")
+require(nycflights13)
 rm(list=ls())
 nycflights13::weather -> weather_all
 y <- dplyr::select(weather_all,
                           origin, time_hour, temp, humid, precip)
-y
+y <- as.data.frame(y)
 # tsdatはoriginがchar, time_hourがdate,
 # それ以外がdoubleになっている。
 
@@ -23,18 +24,20 @@ tbl_notwork
 #ts1 <- ts(1:10, start=c(1990,1), freq=4)
 #ts2 <- ts(10:1, start=c(1990,1), freq=4)
 #y <- ts.union(ts1,ts2)
+
 class(y)
+head(y)
 if (is.ts(y)) {
   print("ts data")
   as.matrix(y) -> y
-} else if (tibble::is_tibble(y)){
-  #  print("tibble")
+} else if (is.data.frame(y) || tibble::is_tibble(y)){
+  # https://www.geeksforgeeks.org/select-only-numeric-columns-from-dataframe-in-r/
+  print("df or tibble")
   dplyr::select_if(y, is.numeric) -> y_tmp
   as.matrix(y_tmp) -> y
-} else
-  # Warning
-  stop("\n The matrix 'y' should be ts or tbl_df.\n")
+  #    browser()
+} else {
+  # Do nothing
 }
-
 head(y)
 class(y)
