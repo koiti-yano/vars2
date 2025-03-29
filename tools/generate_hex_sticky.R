b393023973e76a84e13a857f93aa86e304362b3d
koiti-yano <koiti@komazawa-u.ac.jp>
2025-03-29 16:30:29 +0900

    hex_vars2a

--- tools/generate_hex_sticky.R
+++ tools/generate_hex_sticky.R
@@ -9,5 +9,5 @@ plot(var_p2.irf)
 
 library(hexSticker)
 s <- sticker(~plot(var_p2.irf),
-             package="vars2", p_size=20, s_x=1.0, s_y=0.8, s_width=2, s_height=1.0,
-             filename="tools/hex_vars2.png")
+             package="vars2a", p_size=20, s_x=1.0, s_y=0.8, s_width=2, s_height=1.0,
+             filename="tools/hex_vars2a.png")
